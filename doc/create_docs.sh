955a70396d5cbefabb0fadd0116cb6ce565bd7f2
Toshiaki Hishinuma <hishinuma@ricos.co.jp>
2019-07-05 15:23:00 +0900

    create manuals

--- doc/create_docs.sh
+++ doc/create_docs.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
+git submodule update -i
+rm -rf doc/*
+
 echo "create developers guide"
 doxygen ./Doxyfile.in
+mv doc/html doc/function_reference
+
+echo "create manual ja"
+cd ./FrontISTR_manuals/markdown_files/ja/
+mkdocs build --clean
+mv site ../../../doc/manual_ja/
+cd -
+
+echo "create manual en"
+cd ./FrontISTR_manuals/markdown_files/en/
+mkdocs build --clean
+mv site ../../../doc/manual_en/
+cd -
